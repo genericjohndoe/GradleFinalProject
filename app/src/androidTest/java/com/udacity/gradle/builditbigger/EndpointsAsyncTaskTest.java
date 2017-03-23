7e7a3e785405b1c0720de7742b4ade7c5ad7bb3b
genericjohndoe <j.robinson.johnson21@gmail.com>
2017-03-22 23:26:00 -0500

    modified test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -1,20 +1,20 @@
 package com.udacity.gradle.builditbigger;
 
+import android.app.Activity;
+
 import org.junit.Test;
 
-import static android.support.test.InstrumentationRegistry.getContext;
 import static junit.framework.Assert.assertNotNull;
 
 /**
  * Created by joeljohnson on 2/26/17.
  */
-public class EndpointsAsyncTaskTest {
+public class EndpointsAsyncTaskTest extends Activity {
 
-    static public String OUTPUT;
     @Test
     public void doInBackground() throws Exception {
        EndpointsAsyncTask task =  new EndpointsAsyncTask();
-               task.execute(new Pair(getContext(),"jay"));
+               task.execute(this);
         String output = task.get();
         assertNotNull(output);
     }
